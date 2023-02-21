5d6a575bbf9ac9a220ba524d32681e0db6126bc4
Mahmoud Mohamed <mahmoudm.abdelwadood@gmail.com>
2023-02-21 18:19:31 +0200

    add BaseApplication

---
+++ app/src/main/java/com/mahmoud/pokemon/BaseApplication.java
@@ -0,0 +1,9 @@
+package com.mahmoud.pokemon;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class BaseApplication extends Application {
+}
